ecbcc1cb88df813ac47a950e175abcdc01af6cfc
Mizok <karasunohina@gmail.com>
2021-10-10 15:31:34 +0800

    build subtree commit

--- build/assets/js/greenScreenKeying.js
+++ build/assets/js/greenScreenKeying.js
@@ -817,6 +817,10 @@ var GreenScreenKeying = /*#__PURE__*/function (_Canvas2DFxBase) {
       this.video.addEventListener('loadeddata', function () {
         // Video is loaded and can be played
         resolve();
+      }, false); // 這一塊主要是for移動裝置, 避免他被點擊放大
+
+      this.video.addEventListener('click', function (e) {
+        e.preventDefault();
       }, false); // body 被按下的時候發動 video的play方法，然後開始canvas的渲染
 
       document.body.addEventListener('click', function () {
@@ -832,9 +836,7 @@ var GreenScreenKeying = /*#__PURE__*/function (_Canvas2DFxBase) {
         _this2.videoStyleWidth = size;
         _this2.videoStyleHeight = vh / vw * size;
         _this2.video.style.width = _this2.videoStyleWidth + 'px';
-        _this2.video.style.height = _this2.videoStyleHeight + 'px';
-        _this2.video.style.pointerEvents = 'none'; // 這一行主要是for移動裝置, 因為移動裝置的video點擊後會自動放大, 這樣就看不到我們的效果了
-        // 創建一個架空的canvas, 把他的長寬設定成跟video現在一樣
+        _this2.video.style.height = _this2.videoStyleHeight + 'px'; // 創建一個架空的canvas, 把他的長寬設定成跟video現在一樣
 
         _this2.virtualCanvas = document.createElement('canvas');
         _this2.virtualCanvas.width = _this2.videoStyleWidth;
